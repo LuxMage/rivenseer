02398a9150e78dfc4770a80148f16d129e6a06d5
LuxMage <luxionaeronia@gmail.com>
2019-05-03 21:43:47 -0400

    Further signal testing

--- Rivenseer.py
+++ Rivenseer.py
@@ -346,23 +346,37 @@ def get_server_data():
 def is_bot(message):
 	return message.author == client.user or message.content.startswith('{0}'.format(serverPrefixes[str(message.guild.id)]))
 	
-def sigterm_handler(signal, frame):
-	'''with open('serverPrefixes.csv', 'w', newline='') as csvfile:
-		prefixWriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		for k, v in serverPrefixes.items():
-			prefixWriter.writerow([k] + [v])
-			
-	with open('serverPlatforms.csv', 'w', newline='') as csvfile:
-		platformWriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		for k, v in serverPlatforms.items():
-			platformWriter.writerow([k] + [v])'''
-			
-	print('Sigterm handler executed! Yay!')
-			
-	sys.exit(0)
+def readConfiguration(signalNumber, frame):  
+    print ('(SIGHUP) reading configuration')
+    return
 
-if __name__ == '__main__':
-	print('Sigterm handler registered!')
-	signal.signal(signal.SIGTERM, sigterm_handler)
+def terminateProcess(signalNumber, frame):  
+    print ('(SIGTERM) terminating the process')
+    sys.exit()
+
+def receiveSignal(signalNumber, frame):  
+    print('Received:', signalNumber)
+    return
+
+if __name__ == '__main__':  
+    # register the signals to be caught
+    #signal.signal(signal.SIGHUP, readConfiguration)
+    signal.signal(signal.SIGINT, receiveSignal)
+    #signal.signal(signal.SIGQUIT, receiveSignal)
+    signal.signal(signal.SIGILL, receiveSignal)
+    #signal.signal(signal.SIGTRAP, receiveSignal)
+    signal.signal(signal.SIGABRT, receiveSignal)
+    #signal.signal(signal.SIGBUS, receiveSignal)
+    signal.signal(signal.SIGFPE, receiveSignal)
+    #signal.signal(signal.SIGKILL, receiveSignal)
+    #signal.signal(signal.SIGUSR1, receiveSignal)
+    signal.signal(signal.SIGSEGV, receiveSignal)
+    #signal.signal(signal.SIGUSR2, receiveSignal)
+    #signal.signal(signal.SIGPIPE, receiveSignal)
+    #signal.signal(signal.SIGALRM, receiveSignal)
+    signal.signal(signal.SIGTERM, terminateProcess)
 
 client.run(TOKEN)
+
+while True:
+	time.sleep(1)
